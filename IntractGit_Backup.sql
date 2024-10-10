1a4b10591f5a32d3b18e99ddd09b6961a3ae5093
MuthuProl-Dev <muthupandi@prolaborate.com>
2024-10-10 15:20:17 +0530

    Add SQL script file

---
+++ IntractGit_Backup.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE [dbo].[usp_GetEmployees]
+AS
+BEGIN
+    SELECT * FROM [dbo].[Employees];
+END;
+GO
+
+
+-- Data for Products
+GO
+
+-- Data for Employees
+INSERT INTO Employees VALUES (2, 'Arshad', 'Ahamed', 'IT')
+GO
+
+
