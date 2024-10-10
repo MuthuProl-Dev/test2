ae57a8830ba07eb9bdbb8badeca47487ad4dc2ef
MuthuProl-Dev <muthupandi@prolaborate.com>
2024-10-10 18:59:44 +0530

    UPDATED QUERY

--- IntractGit_Backup.sql
+++ IntractGit_Backup.sql
@@ -3,8 +3,6 @@ AS
 BEGIN
     SELECT * FROM [dbo].[Employees];
 END;
-GO
-
 
 -- Data for Employees
 INSERT INTO [dbo].[Employees] (ID, FirstName, LastName, Department) VALUES (2, 'Muthu', 'Pandi', 'IT');
