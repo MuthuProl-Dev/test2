83d4a6bdcd05bcaf7c759ec8e579e477240ae96c
MuthuProl-Dev <muthupandi@prolaborate.com>
2024-10-11 12:36:59 +0530

    query updated

--- IntractGit_Backup.sql
+++ IntractGit_Backup.sql
@@ -1,9 +1,2 @@
-CREATE PROCEDURE [dbo].[usp_GetEmployees]
-AS
-BEGIN
-    SELECT * FROM [dbo].[Employees];
-END;
-
--- Data for Employees
 INSERT INTO [dbo].[Employees] (ID, FirstName, LastName, Department) VALUES (3, 'Arshad', 'Ahamed', 'IT');
 
